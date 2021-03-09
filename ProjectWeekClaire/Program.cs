476f9dc8f68566fdce6cdb1f994702138df9c3e8
Claire Whitten <claire.whitten@student.kdg.be>
2021-03-09 11:23:32 +0100

    09/03 project updated

--- ProjectWeekClaire/Program.cs
+++ ProjectWeekClaire/Program.cs
@@ -10,13 +10,263 @@ namespace ProjectWeekClaire
 {
     class Program
     {
-        
 
+        static Random rGenerator = new Random();
+
+        //questions  - bewerken? encrypting ++? balance as global variable? regex order problem? alt-codes?
         static void Main(string[] args)
         {
-            showMenu();
+        
+         
+           
 
-            int balance = 200; //why is this not global
+            //showMenu();
+
+            int balance = 200; //to make global  - pass it through as parameter 
+
+
+
+
+            /* BLACKJACK GAME
+
+            string[] kaarten = { "A♥", "2♥", "3♥", "4♥", "5♥", "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥",
+                "A♦", "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦",
+            "A♣", "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣",
+            "A♠", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠"};
+
+            int[] values = {11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
+            11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
+            11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
+            11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,};
+
+            //variables 
+            string spelerCurrentHand = "";
+            string dealerCurrentHand = "";
+            int spelerTotal = 0;
+            int dealerTotal = 0;
+            string choice = "";
+            List<int> cardsDrawn = new List<int>();  //list because don't know how long it will be in advance 
+            
+
+            balance = balance - 10;
+
+            //instructions
+            Console.WriteLine("Welcome to Blackjack!         To draw a card press D, to stop drawing press S \n");
+
+            /*Console.WriteLine("A blackjack pot costs $10.");
+            Console.WriteLine("The goal is to beat the dealer. Whoever scores the highest number closest to 21 wins!");
+            Console.WriteLine("If your total is greater than 21 you lose!");
+            Console.WriteLine("You can choose to keep drawing(d) or stop!(s)");
+
+           
+
+            Console.WriteLine("Your starting hand: \n");
+
+
+            for (int i = 0; i < 2; i++)
+            {
+                int randomNumber = rGenerator.Next(0, 52);
+                spelerCurrentHand = spelerCurrentHand + kaarten[randomNumber] + "    ";
+                cardsDrawn.Add(values[randomNumber]); 
+            }
+            
+            spelerTotal = cardsDrawn.Sum();
+            Console.WriteLine($"{spelerCurrentHand}                                 Total:{spelerTotal} \n"); 
+
+
+            if (spelerTotal == 21)
+            {
+                Console.WriteLine("Wow! Your first 2 cards total 21. You win $25!");
+                balance = balance + 25;
+            }
+
+
+            while (spelerTotal < 21 && choice != "S")
+            {
+                Console.Write("Draw(D)/Stop(S):");
+                choice = Console.ReadLine().ToUpper();
+
+                if (choice == "D")
+                {
+                    int randomNumber = rGenerator.Next(0, 52);
+                    spelerCurrentHand = spelerCurrentHand + kaarten[randomNumber] + "    ";
+                    cardsDrawn.Add(values[randomNumber]);
+                    spelerTotal = cardsDrawn.Sum();
+                    if (spelerTotal > 21)
+                    {
+                        for (int i = 0; i < cardsDrawn.Count; i++)
+                        {
+                            if (cardsDrawn[i] == 11)
+                            {
+                                cardsDrawn[i] = 1;
+                            }
+                        }
+                        spelerTotal = cardsDrawn.Sum();
+                    }
+                    Console.WriteLine($"{spelerCurrentHand}                          Total:{spelerTotal}");
+                }
+                if (choice == "S")
+                {
+                    Console.WriteLine($"You have stopped. Your final total is {spelerTotal}");
+                    dealerTotal = dealersTurn(dealerTotal, dealerCurrentHand, kaarten, values);
+
+                }
+              
+            }
+
+            if (spelerTotal >= 21)
+            {
+                Console.WriteLine($"Your total has reached 21 or over. Your final total is {spelerTotal}");
+                dealerTotal = dealersTurn(dealerTotal, dealerCurrentHand, kaarten, values);
+            }
+
+
+
+
+            //check rules again in document 
+            if (spelerTotal > 21 && dealerTotal <= 21)
+            {
+                Console.WriteLine("You Lose! You have more than 21.");
+
+            } else if (spelerTotal <= 21 && (spelerTotal > dealerTotal || dealerTotal > 21))
+            {
+                Console.WriteLine("You win! You receive $20 and your $10 back!"); //? + 20 or 30
+                balance = balance + 30;
+
+            } else if (spelerTotal < dealerTotal && dealerTotal <= 21)
+            {
+                Console.WriteLine("You lose! The dealer has more.");
+
+            } else if (spelerTotal == dealerTotal || (dealerTotal > 21 && spelerTotal > 21))
+            {
+                Console.WriteLine("It's a draw between you and the dealer! You both have more than 21.");
+                Console.WriteLine("You get your $10 back!");  // is this needed? 
+                balance = balance + 10;
+            }
+
+            
+            gameMenu(balance);
+
+            */
+
+            // Fruit machine 
+            //Random rnd = new Random();
+            //string[] MyRandomArray = MyArray.OrderBy(x => rnd.Next()).ToArray();
+
+
+
+            int random;
+
+           
+
+            string[] symbols = {"☺", "♠", "♣", "♦", "♥", "A", "7"};
+
+            string[] fruitMachine = new string[9];
+
+            
+            
+            string answer = "";
+            do
+            {
+                balance = balance - 5;
+
+                Console.WriteLine($"Welcome to the Slot Machine!");
+                Console.WriteLine("Get 3 of the same symbols horizontally or diagonally to win!");
+
+               
+                for (int i = 0; i < fruitMachine.Length; i++)
+                {
+                    random = rGenerator.Next(0, 7);
+                    fruitMachine[i] = symbols[random]; 
+                }
+
+                /*for (int i = 0; i < fruitMachine.Length; i++)
+                { 
+                    Console.Clear();
+                    Console.WriteLine($"{fruitMachine[i]} {fruitMachine[i+1]} {fruitMachine[i+2]}");
+                }*/
+                
+                
+                Console.WriteLine($"{fruitMachine[0]} {fruitMachine[1]} {fruitMachine[2]}");
+
+                Console.WriteLine($"{fruitMachine[3]} {fruitMachine[4]} {fruitMachine[5]}");
+
+                Console.WriteLine($"{fruitMachine[6]} {fruitMachine[7]} {fruitMachine[8]}");
+
+
+                string symbol;
+                int total = 0;
+                for (int i = 0; i < fruitMachine.Length; i++)
+                {
+                    //check for horizontal rows and update total
+                    if (i == 0 || i == 3 || i == 6)
+                    {
+                        if (fruitMachine[i] == fruitMachine[i+1] && fruitMachine[i+1] == fruitMachine[i + 2])
+                        {
+                            symbol = fruitMachine[i];
+                            total = symbolCheck(symbol, total, balance);
+                            
+
+                        }
+                    }
+                    //check for diagonal rows and update total
+                    if (i == 0 || i == 7)
+                    {
+                        if (i == 0)
+                        {
+                            if (fruitMachine[i] == fruitMachine[i + 4] && fruitMachine[i+4] == fruitMachine[i + 8])
+                            {
+                                symbol = fruitMachine[i];
+                                total = symbolCheck(symbol,total, balance);
+                            }
+                        }
+                        if (i == 6)
+                        {
+                            if (fruitMachine[i] == fruitMachine[i -2] && fruitMachine[i - 2] == fruitMachine[i + 4])
+                            {
+                                symbol = fruitMachine[i];
+                                total = symbolCheck(symbol, total, balance);
+                            }
+                        }
+                    }
+                }
+                //check if no rows
+                if (total == 0)
+                {
+                    Console.WriteLine($"No rows! Sorry you lose your $5.             Your balance: {balance}");
+                }
+
+                balance = balance + total;
+
+                Console.WriteLine("Do you want to play again?");
+                answer = Console.ReadLine().ToUpper();
+
+                Console.Clear(); 
+
+                
+
+            } while (answer == "Y");
+
+
+            gameMenu(balance);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -30,6 +280,75 @@ namespace ProjectWeekClaire
         //METHODS 
 
         //Main menu
+
+
+        static int dealersTurn(int dealerTotal, string dealerCurrentHand, string[] kaarten, int[] values)
+        {
+           
+            while (dealerTotal < 17)
+            {
+                int randomNumber = rGenerator.Next(0, 52);
+                dealerCurrentHand = dealerCurrentHand + kaarten[randomNumber] + "    ";
+                dealerTotal = dealerTotal + values[randomNumber];
+                Console.WriteLine($"{dealerCurrentHand}                            Total:{dealerTotal}");
+                
+            }
+
+            Console.WriteLine("The dealer's final score: ");
+            Console.WriteLine($"{dealerCurrentHand}                                Total:{dealerTotal}");
+            return dealerTotal;
+        }
+
+
+        static int symbolCheck(string symbol, int total, int balance)
+        {
+            switch (symbol)
+            {
+                case "☺":
+                    total = total + 3;
+                    balance = balance + total;
+                    Console.WriteLine($"You win $3                    Your balance: {balance}");
+                    break;
+                case "♠":
+                    total = total + 5;
+                    balance = balance + total;
+                    Console.WriteLine($"You win $5                    Your balance: {balance} ");
+                    break;
+                case "♣":
+                   
+                    total = total + 7;
+                    balance = balance + total;
+                    Console.WriteLine($"You win $7                    Your balance: {balance}");
+                    break;
+                case "♦":
+                    
+                    total = total + 10;
+                    balance = balance + total;
+                    Console.WriteLine($"You win $10                   Your balance: {balance}");
+                    break;
+                case "♥":
+                    
+                    total = total + 20;
+                    balance = balance + total;
+                    Console.WriteLine($"You win $20                   Your balance: {balance}");
+                    break;
+                case "A":
+                    
+                    total = total + 50;
+                    balance = balance + total;
+                    Console.WriteLine($"You win $50                   Your balance: {balance}");
+                    break;
+                case "7":
+                    
+                    total = total + 100;
+                    balance = balance + total;
+                    Console.WriteLine($"You win $100                  Your balance: {balance}");
+                    break;
+            }
+            return total;
+
+        }
+
         static void showMenu()
         {
 
@@ -49,6 +368,23 @@ namespace ProjectWeekClaire
             }
         }
 
+
+
+        //show game menu
+        static void gameMenu(int balance =200, string username = "Unknown user")
+        {
+           
+            Console.WriteLine($"WELCOME {username}   :)");
+            //Console.WriteLine($"{date}, {time}   Je bent {length} ingelogd.");
+            Console.WriteLine($"Je hebt nu ${balance} om me te spelen!");
+            Console.WriteLine("Wat wil je doen? Kies een optie:");
+            Console.WriteLine("1. Play Blackjack");
+            Console.WriteLine("2. Play slot machine");
+            Console.WriteLine("3. Play memory");
+            Console.WriteLine("4. Uitloggen");
+
+
+        }
 
 
 
@@ -86,6 +422,8 @@ namespace ProjectWeekClaire
                     Console.WriteLine("Password:");
                     string password = Console.ReadLine();
                     Regex passwordPattern = new Regex(@"[a-z]+[A-Z]+[0-9]+[^a-zA-Z0-9]+");// only working in this order - how to change??? can you check length with reg ex   At least one special character [*.!@#$%^&(){}[]:;<>,.?/~_+-=|\]
+                    //change to  if password.Any(char klein) && pasword.Any(char hoofd) && password.Any(int number) && regex for symbols only  - then it can be in any order 
+                    
                     if (passwordPattern.IsMatch(password) && password.Length >= 8 && password.Length <= 20)
                     {
                         Console.WriteLine("Valid password!");
@@ -167,7 +505,7 @@ namespace ProjectWeekClaire
             {
                 if (passwordMatch(returnedUserDetails, userName))
                 {
-                    GameMenu(userName,200);
+                    gameMenu(200, userName);
                 }
             }
             else
@@ -178,21 +516,7 @@ namespace ProjectWeekClaire
            
         }
 
-        //show game menu
-        static void GameMenu( string username, int balance)
-        {
-            Console.Clear();
-            Console.WriteLine($"WELCOME {username}   :)");
-            //Console.WriteLine($"{date}, {time}   Je bent {length} ingelogd.");
-            Console.WriteLine($"Je hebt ${balance} om me te spelen!");
-            Console.WriteLine("Wat wil je doen? Kies een optie:");
-            Console.WriteLine("1. Play Blackjack");
-            Console.WriteLine("2. Play slot machine");
-            Console.WriteLine("3. Play memory");
-            Console.WriteLine("4. Uitloggen");
-            
-           
-        }
+        
 
 
         //Edit a username and password  - MAKE METHODS FOR NEW USERNAME AND PASSWORD REGEX CHECK
